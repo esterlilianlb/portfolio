d5a5e2323bddf9ff59d2f20bf08513502b6a8860
esterlilianlb <esterlilian2231@gmail.com>
2020-07-05 17:10:15 -0300

    progress bar adicionadas, imagens de projetos no assets

---
+++ src/projectCards.js
@@ -0,0 +1,20 @@
+const projectList = PROJECTS.projects;
+const divCards = document.querySelector('.cards-container');
+
+function printCards() {
+
+  projectList.map(item => {
+    divCards.innerHTML += `
+    <div class="project">
+      <img src="${item.img}">
+      <h4>${item.titulo}</h4>
+      <p>${item.stack}</p>
+      <p>${item.sobre}</p>
+      <a href="${item.link}">Visite a página</p>
+      <a href="">Github</a>
+    </div>
+    `
+  })
+
+}
+printCards();
